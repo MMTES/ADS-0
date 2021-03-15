fb7c43bff9c0eccde311a85b5a89b4862dd88d9e
MMTES <42440704+MMTES@users.noreply.github.com>
2021-03-15 19:58:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,6 @@
 // Copyright 2021 NNTU-CS
 int gcd(int a, int b) {
-    if (a*b == 0) return a+b;
-    if (a < b)
-        return gcd(a, b%a);
-    else
-        return gcd(a%b, b);
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
 }
